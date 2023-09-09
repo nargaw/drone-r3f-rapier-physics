1c3fb1f2230105a68789840a7a376012c0334f51
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2023-09-09 05:00:35 -0400

    fixed and revolute joints

--- src/TestDrone.jsx
+++ src/TestDrone.jsx
@@ -1,5 +1,5 @@
 import { useFrame } from "@react-three/fiber"
-import { RigidBody, CuboidCollider } from "@react-three/rapier"
+import { RigidBody, CuboidCollider, useRevoluteJoint, useFixedJoint } from "@react-three/rapier"
 import { useEffect, useRef, useState } from "react"
 import * as THREE from 'three'
 import { useKeyboardControls } from "@react-three/drei"
@@ -11,6 +11,9 @@ export default function TestDrone()
     const roterBottomRight = useRef()
     const roterTopLeft = useRef()
     const roterBottomLeft = useRef()
+    const landingLeft = useRef()
+    const landingRight = useRef()
+
     console.log(roterTopRight.current)
 
     const [subscribeKeys, getKeys] = useKeyboardControls()
@@ -23,6 +26,54 @@ export default function TestDrone()
 
     const [ smoothedCameraPosition ] = useState(() => new THREE.Vector3(10, 50, 10))
     const [ smoothedCameraTarget ] = useState(() => new THREE.Vector3())
+
+    const STIFFNESS = 150000
+    const DAMPING = 2
+    const joint = useRevoluteJoint(obj, roterTopLeft, [
+        [-0.5 , 0, -1],
+        [0, 0, 0],
+        [0, 2, 0]
+    ])
+
+    // const joint2 = useRevoluteJoint(obj, roterTopRight, [
+    //     [0.5, 0., -1],
+    //     [0, 0, 0],
+    //     [0, 1, 0]
+    // ])
+
+    // const joint3 = useRevoluteJoint(obj, roterBottomLeft, [
+    //     [0, 0, 0],
+    //     [-1, 1, 1],
+    //     [0, 1, 0]
+    // ])
+
+    // const joint4 = useRevoluteJoint(obj, roterBottomRight, [
+    //     [0, 0, 0],
+    //     [1, 1, 1],
+    //     [0, 1, 0]
+    // ])
+
+    const fixedJointLeft = useFixedJoint(obj, landingLeft, [
+        [0, 0, 0],
+        [0, 0, 0, 1],
+        [0, 0, 0],
+        [0, 0, 0, 1]
+    ])
+
+    const fixedJointRight = useFixedJoint(obj, landingRight, [
+        [0, 0, 0],
+        [0, 0, 0, 1],
+        [0, 0, 0],
+        [0, 0, 0, 1]
+    ])
+    
+    
+    useEffect(() => {
+        console.log(joint.current)
+        // joint.current?.configureMotorPosition(0.3, STIFFNESS, DAMPING)
+        
+    })
+  
 
     useFrame((state, delta) => {
         const bodyPosition = obj.current.translation()
@@ -53,6 +104,12 @@ export default function TestDrone()
         if(upward)
         {
             force.y += forceStrength
+            // console.log(state.clock.elapsedTime)
+            joint.current?.configureMotorVelocity(forceStrength * 550, DAMPING)
+            joint.current?.anchor1(new THREE.Vector3(0, 1, 0))
+            obj.current?.wakeUp()
+            roterTopLeft.current?.wakeUp()
+
         }
 
         
@@ -84,51 +141,65 @@ export default function TestDrone()
             torque.z += torqueStrength
         }
 
-        obj.current.applyImpulse(impulse)
-        obj.current.applyTorqueImpulse(torque)
-        obj.current.addForce(force)
+        // obj.current.applyImpulse(impulse)
+        // obj.current.applyTorqueImpulse(torque)
+        // obj.current.addForce(force)
 
-        roterBottomRight.current.rotation.y += 2 * delta * Math.random()
-        roterBottomLeft.current.rotation.y += 2 * delta * Math.random()
-        roterTopRight.current.rotation.y += 2 * delta * Math.random()
-        roterTopLeft.current.rotation.y += 2 * delta * Math.random()
+        // roterBottomRight.current.rotation.y += 2 * delta * Math.random()
+        // roterBottomLeft.current.rotation.y += 2 * delta * Math.random()
+        // roterTopRight.current.rotation.y += 2 * delta * Math.random()
+        // roterTopLeft.current.rotation.y += 2 * delta * Math.random()
     })
 
     return(
         <>
-            <RigidBody colliders={'cuboid'} restitution={0.5} ref={obj} onClick={jump}>
+            {/* <RigidBody colliders={'cuboid'} restitution={0.5} ref={obj} onClick={jump}> */}
                 <group>
-                    <mesh position={[-0.25, -0.25, 0]}> 
-                        <boxGeometry args={[0.1, 0.5, 0.5]} />
-                        <meshNormalMaterial />
-                    </mesh>
-                    <mesh position={[0.25, -0.25, 0]}> 
-                        <boxGeometry args={[0.1, 0.5, 0.5]} />
-                        <meshNormalMaterial />
-                    </mesh>
-                    <mesh position={[0.5, 0.2, 0.5]} ref={roterBottomRight}> 
-                        <boxGeometry args={[0.1, 0.01, 0.75]} />
-                        <meshStandardMaterial color='green' />
-                    </mesh>
-                    <mesh position={[-0.5, 0.2, 0.5]} ref={roterBottomLeft}> 
-                        <boxGeometry args={[0.1, 0.01, 0.75]} />
-                        <meshStandardMaterial color='red' />
-                    </mesh>
-                    <mesh position={[0.5, 0.2, -0.5]} ref={roterTopRight}> 
-                        <boxGeometry args={[0.1, 0.01, 0.75]} />
-                        <meshStandardMaterial color='orange' />
-                    </mesh>
-                    <mesh position={[-0.5, 0.2, -0.5]} ref={roterTopLeft}> 
-                        <boxGeometry args={[0.1, 0.01, 0.75]} />
-                        <meshStandardMaterial color='blue' />
-                    </mesh>
-                    <mesh castShadow >
-                        <boxGeometry args={[1, 0.2, 1]}/>
-                        <meshNormalMaterial />
-                    </mesh>
+                    <RigidBody colliders='cuboid' ref={landingLeft}>
+                        <mesh position={[-0.25, -0.25, 0]}> 
+                            <boxGeometry args={[0.1, 0.5, 0.5]} />
+                            <meshNormalMaterial />
+                        </mesh>
+                    </RigidBody>
+                    <RigidBody ref={landingRight}>
+                        <mesh position={[0.25, -0.25, 0]}> 
+                            <boxGeometry args={[0.1, 0.5, 0.5]} />
+                            <meshNormalMaterial />
+                        </mesh>
+                    </RigidBody>
+                    <RigidBody ref={roterBottomRight}>
+                        <mesh position={[0.5, 0.2, 0.5]} > 
+                            <boxGeometry args={[0.1, 0.01, 0.75]} />
+                            <meshStandardMaterial color='green' />
+                        </mesh>
+                    </RigidBody>
+                    <RigidBody ref={roterBottomLeft}>
+                        <mesh position={[-0.5, 0.2, 0.5]} > 
+                            <boxGeometry args={[0.1, 0.01, 0.75]} />
+                            <meshStandardMaterial color='red' />
+                        </mesh>
+                    </RigidBody>
+                    <RigidBody ref={roterTopRight}>
+                        <mesh position={[0.5, 0.2, -0.5]} > 
+                            <boxGeometry args={[0.1, 0.01, 0.75]} />
+                            <meshStandardMaterial color='orange' />
+                        </mesh>
+                    </RigidBody>
+                    <RigidBody ref={roterTopLeft}>
+                        <mesh position={[-0.5, 0.2, -0.5]} > 
+                            <boxGeometry args={[0.1, 0.01, 0.75]} />
+                            <meshStandardMaterial color='blue' />
+                        </mesh>
+                    </RigidBody>
+                    <RigidBody ref={obj} onClick={jump}>
+                        <mesh castShadow >
+                            <boxGeometry args={[1, 0.2, 1]}/>
+                            <meshNormalMaterial />
+                        </mesh>
+                    </RigidBody>   
                 </group>
                 
-            </RigidBody>
+            {/* </RigidBody> */}
             
         </>
     )
